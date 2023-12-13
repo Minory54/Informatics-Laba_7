42379759a59c07495be7c0ae73f77f24684f5e83
Minory54 <72006176+Minory54@users.noreply.github.com>
2023-12-14 00:03:35 +0700

    Добавьте файлы проекта.

---
+++ N1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace N1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Random rand = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int readInt(TextBox textBox)
+        {
+            int intNum;
+
+            while (!(int.TryParse(textBox.Text, out intNum)))
+            {
+                MessageBox.Show("Неверно задано число.", "Ошибка!");
+                textBox.BorderBrush = Brushes.Red;
+                return 0;
+            }
+            textBox.BorderBrush = Brushes.Green;
+            return intNum;
+        }
+
+        string genPassword(int start, int end, int len) 
+        {
+            string result = "";
+
+            for (int i = 0; i < len; i++)
+            {
+                string randStr = rand.Next(start, end).ToString();
+                result = result + char.ConvertFromUtf32(Convert.ToInt32(randStr)).ToString();
+            }
+
+            return result;
+        }
+
+        private void btn_gen_Click(object sender, RoutedEventArgs e)
+        {
+
+            int start = int.Parse(tb_start.Text, System.Globalization.NumberStyles.HexNumber);
+            int end = int.Parse(tb_end.Text, System.Globalization.NumberStyles.HexNumber);
+            int len = readInt(tb_len);
+
+            tb_password.Text = genPassword(start, end, len);            
+        }
+    }
+}
---
+++ N2/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace N2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int readInt(TextBox textBox)
+        {
+            int intNum;
+
+            while (!(int.TryParse(textBox.Text, out intNum))) // Проверка введенной строки на тип int
+            {
+                MessageBox.Show("Неверно задано число.", "Ошибка!");
+                textBox.BorderBrush = Brushes.Red;
+                return 0;
+            }
+            textBox.BorderBrush = Brushes.Green;
+            return intNum;
+        }
+
+        private void btn_load_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fs = File.OpenRead(dlg.FileName);
+                byte[] array = new byte[fs.Length];
+                fs.Read(array, 0, array.Length); 
+                string textFromFile = System.Text.Encoding.UTF8.GetString(array); 
+                tb_result.Text = textFromFile;
+            }
+            
+        }
+
+        string encrypt(TextBox textBox, int key) // шифровка
+        {
+            string text = tb_result.Text;
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                byte symbol;
+                int a = Convert.ToInt32(text[i]);
+                symbol = (byte)(a + key);
+
+                result += char.ConvertFromUtf32(Convert.ToInt32(symbol)).ToString();
+            }
+
+            return result;
+        }
+
+        string decipher(TextBox textBox, int key) // дешифровка
+        {
+            string text = tb_result.Text;
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                byte symbol;
+                int a = Convert.ToInt32(text[i]);
+                symbol = (byte)(a - key);
+
+                result += char.ConvertFromUtf32(Convert.ToInt32(symbol)).ToString();
+            }
+
+            return result;
+        }
+
+        private void btn_encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            int key = readInt(tb_key);
+            tb_result.Text = encrypt(tb_result, key);
+        }
+
+        private void btn_decipher_Click(object sender, RoutedEventArgs e)
+        {
+            int key = readInt(tb_key);
+            tb_result.Text = decipher(tb_result, key);
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            string text = tb_result.Text;
+
+            if (dlg.ShowDialog() == true)
+            {
+                using (FileStream fstream = new FileStream(dlg.FileName, FileMode.OpenOrCreate))
+                {
+                    byte[] array = System.Text.Encoding.UTF8.GetBytes(text);
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+
+        }
+    }
+}
